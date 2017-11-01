3089d9d5a39e05dbcdc13973bae2143b583e100c
ajung13 <gkrhvos>
2017-11-01 13:48:37 +0900

    Initial commit 1. main: Show map with my position 2. link mainActivity to writingActivity

---
+++ app/src/main/java/ac/sogang/dangol/MainActivity.java
@@ -0,0 +1,146 @@
+package ac.sogang.dangol;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        checkDangerousPermissions();
+    }
+
+    public void onWriteClicked(View v){
+        Intent intent = new Intent(MainActivity.this, WritingActivity.class);
+        startActivity(intent);
+        return;
+    }
+
+    private void checkDangerousPermissions(){
+        String[] permissions = {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        };
+
+        int permissionCheck = PackageManager.PERMISSION_GRANTED;
+        for(int i = 0; i < permissions.length; i++){
+            permissionCheck = ContextCompat.checkSelfPermission(this, permissions[i]);
+            if(permissionCheck == PackageManager.PERMISSION_DENIED)
+                break;
+        }
+
+        if(permissionCheck == PackageManager.PERMISSION_GRANTED)
+            Log.e("dangol_main", "Permission granted");
+//            Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+        else{
+//            Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+            Log.e("dangol_main", "Permission denied");
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[0]))
+                Toast.makeText(this, "Explain for permission", Toast.LENGTH_SHORT).show();
+            else
+                ActivityCompat.requestPermissions(this, permissions, 1);
+        }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            return;
+
+        mMap.setMyLocationEnabled(true);
+
+        LocationManager manager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        GPSListener gpsListener = new GPSListener();
+        long minTime = 10000;
+        float minDistance = 0;
+
+        try{
+            manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, gpsListener);
+            manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, minTime, minDistance, gpsListener);
+
+            Location lastlocation = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if(lastlocation != null){
+                Double latitude = lastlocation.getLatitude();
+                Double longitude = lastlocation.getLongitude();
+                Log.e("dangol_main", "Last known location: " + latitude + "\t" + longitude);
+            }
+        }catch(SecurityException se){
+            Log.e("dangol_main", "catch " + se.getMessage());
+        }
+
+        Toast.makeText(this, "Check log", Toast.LENGTH_SHORT).show();
+
+        // Add a marker in Sydney and move the camera
+/*        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        if(requestCode == 1){
+            for(int i=0; i<permissions.length; i++){
+                if(grantResults[i] == PackageManager.PERMISSION_GRANTED)
+                    Toast.makeText(this, permissions[i] + " permission granted", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(this, permissions[i] + " permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private class GPSListener implements LocationListener{
+        public void onLocationChanged(Location location){
+            Double latitude = location.getLatitude();
+            Double longitude = location.getLongitude();
+
+            Log.e("dangol_main", "Location Changed: " + latitude + "\t" + longitude);
+
+            LatLng myLocation = new LatLng(latitude, longitude);
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 19));
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
+        public void onProviderDisabled(String provider){}
+        public void onProviderEnabled(String provider){}
+        public void onStatusChanged(String provider, int status, Bundle extras){}
+    }
+}
