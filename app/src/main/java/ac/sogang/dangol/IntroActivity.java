58296d3ac94b3bafeac895877b804abd374c141b
ajung13 <gkrhvos>
2017-11-08 13:54:32 +0900

    1108 Add intro & Select date(calendar)

---
+++ app/src/main/java/ac/sogang/dangol/IntroActivity.java
@@ -0,0 +1,49 @@
+package ac.sogang.dangol;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class IntroActivity extends AppCompatActivity {
+    String dbName = "Dangol";
+    String[] tableName = {"tmpLocation", "visitedLocation", "Diary"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+//        makeTables();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
+    }
+
+    private void makeTables(){
+        SQLiteDatabase mDB = this.openOrCreateDatabase(dbName, MODE_PRIVATE, null);
+        try{
+            for(String tName : tableName){
+                //----MUST FILL THIS FIELD----
+                String sql = "create table if not exists " + tName + "";
+                mDB.execSQL(sql);
+            }
+        }catch(SQLiteException se){
+            Log.e("dangol_intro", "SQLite exception - " + se.toString());
+        }catch(Exception e) {
+            Log.e("dangol_intro", "Exception - " + e.toString());
+        }
+
+        mDB.close();
+    }
+}
--- app/src/main/java/ac/sogang/dangol/WritingActivity.java
+++ app/src/main/java/ac/sogang/dangol/WritingActivity.java
@@ -1,18 +1,23 @@
 package ac.sogang.dangol;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class WritingActivity extends AppCompatActivity {
-    String date = "";
+    int _year = 0;
+    int _month = 0;
+    int _date = 0;
     int emotion = 0;
     int weather = 0;
     String title = "";
@@ -22,6 +27,11 @@ public class WritingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_writing);
+
+        Date date_now = new Date(System.currentTimeMillis());
+        _year = Integer.parseInt(new SimpleDateFormat("yyyy").format(date_now));
+        _month = date_now.getMonth() + 1;
+        _date = date_now.getDate();
         setDate();
     }
 
@@ -54,17 +64,29 @@ public class WritingActivity extends AppCompatActivity {
     }
 
     private void uploadDB(){
-        Toast.makeText(getApplicationContext(), "날짜: " + date + ", 기분: " + emotion + ", 날씨: " + weather,
+        Toast.makeText(getApplicationContext(), "기분: " + emotion + ", 날씨: " + weather,
                 Toast.LENGTH_SHORT).show();
         Toast.makeText(getApplicationContext(), "제목: " + title + ", 내용: " + contents, Toast.LENGTH_SHORT).show();
     }
 
     private void setDate(){
-        Date date_now = new Date(System.currentTimeMillis());
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        date = dateFormat.format(date_now);
-
-        TextView tv = (TextView)findViewById(R.id.write_calendar);
-        tv.setText(date);
+        Button tv = (Button)findViewById(R.id.write_calendar);
+        String tvPrint = _year + "-" + _month + "-" + _date;
+        tv.setText(tvPrint);
     }
+
+    public void onCalendarClicked(View v){
+        DatePickerDialog dialog = new DatePickerDialog(this, listener, _year, _month - 1, _date);
+        dialog.show();
+    }
+    private DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            _year = year;
+            _month = month + 1;
+            _date = dayOfMonth;
+            Log.e("dangol_writing", "new date set: " + year + "-" + month + "-" + dayOfMonth);
+            setDate();
+        }
+    };
 }
