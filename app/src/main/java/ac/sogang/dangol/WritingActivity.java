27c7244152891246761c0408b3626adb680e6754
ajung13 <gkrhvos>
2017-11-08 11:36:40 +0900

    1106 writing activity (test branch)

--- app/src/main/java/ac/sogang/dangol/WritingActivity.java
+++ app/src/main/java/ac/sogang/dangol/WritingActivity.java
@@ -22,7 +22,6 @@ public class WritingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_writing);
-
         setDate();
     }
 
